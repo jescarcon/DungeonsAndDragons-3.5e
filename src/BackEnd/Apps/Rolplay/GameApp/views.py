9983dec9eb07288a3917da43c394b84f4bbfb448
jescarcon <jesuscardenas2000@gmail.com>
2024-11-06 00:48:08 +0100

    QuickFix BackEnd:GameApp Permissions in views

--- src/BackEnd/Apps/Rolplay/GameApp/views.py
+++ src/BackEnd/Apps/Rolplay/GameApp/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import Game, Note, Diary, DiaryEntry, Image
 from .serializers import GameSerializer, NoteSerializer, DiarySerializer, DiaryEntrySerializer, ImageSerializer
+from rest_framework.permissions import IsAuthenticated
 
     # Django Rest Framework creates CRUD
 
@@ -8,24 +9,31 @@ from .serializers import GameSerializer, NoteSerializer, DiarySerializer, DiaryE
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+    permission_classes = [IsAuthenticated]  
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user) 
 
 #---------------------NOTES VIEWSET---------------------
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
- 
+    permission_classes = [IsAuthenticated]  
+
 #---------------------DIARY VIEWSET---------------------
 class DiaryViewSet(viewsets.ModelViewSet):
     queryset = Diary.objects.all()
     serializer_class = DiarySerializer
- 
+    permission_classes = [IsAuthenticated]  
+
 #---------------------DIARY ENTRY VIEWSET---------------------
 class DiaryEntryViewSet(viewsets.ModelViewSet):
     queryset = DiaryEntry.objects.all()
     serializer_class = DiaryEntrySerializer
- 
+    permission_classes = [IsAuthenticated]  
+
 #---------------------IMAGE VIEWSET---------------------
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
- 
+    permission_classes = [IsAuthenticated]  
